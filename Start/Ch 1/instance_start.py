f70803c29ff6faabf9a745937f44ed37e831f427
Hugh Geller <geller.hugh@gmail.com>
2024-11-06 21:11:44 +0000

    learned double underscore

--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -11,6 +11,7 @@ class Book:
         self.author = author
         self.pages = pages
         self.price = price
+        self.__secret = "This is a secret attribute"
 
     # TODO: create instance methods
     def getprice(self):
@@ -28,11 +29,12 @@ b1 = Book("War and Peace", "Leo Tolstoy", 1225,39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
 
 # TODO: print the price of book1
-print(b1.getprice())
+# print(b1.getprice())
 
 # TODO: try setting the discount
-print(b2.getprice())
-b2.setdiscount(0.25)
-print(b2.getprice())
+# print(b2.getprice())
+# b2.setdiscount(0.25)
+# print(b2.getprice())
 
 # TODO: properties with double underscores are hidden by the interpreter
+print(b2._Book__secret)
